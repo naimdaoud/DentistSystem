05f71376c877b9ce54b86be353ab4367dcf95746
Naim Daoud <53578912+naimdaoud@users.noreply.github.com>
2023-04-29 13:48:00 +0400

    Calendar

--- DentistSystem/Calendar.Designer.vb
+++ DentistSystem/Calendar.Designer.vb
@@ -22,9 +22,16 @@ Partial Class Calendar
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Calendar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "Calendar"
         Me.Text = "Calendar"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
--- DentistSystem/Calendar.vb
+++ DentistSystem/Calendar.vb
@@ -1,3 +1,6 @@
-﻿Public Class Calendar
+﻿Imports Calendar.DayView
+Public Class Calendar
+    Private Sub Calendar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
